5a7d67a46c8810402150cf93fa5d46449502ecf3
TheJoker-IOS <68492377+TheJoker-IOS@users.noreply.github.com>
2021-10-21 18:39:17 -0400

    kkkk

--- depictions/js/setDepiction.js
+++ depictions/js/setDepiction.js
@@ -1,60 +1,70 @@
 
 $(function(){
     var bundle = getQueryVariable('p');
-
+    
     if(bundle != undefined){
         //Now fetch the appropriate file from this query string
     }
-
-
+    
+    
     console.log(getQueryVariable('p'));
     console.log("Fetching XML");
     var getUrl = window.location;
     var baseUrl = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
     console.log(baseUrl + "/"+ bundle + "/info.xml");
-
+    
     $.ajax({
-        type: "GET",
-        url: baseUrl + "/" + bundle + "/info.xml",
-        dataType: "xml",
-        success: function (xml) {
-            console.log("Beginning XML Parsing");
-
-            // Parse the xml file and get data
+    type: "GET",
+    url: baseUrl + "/" + bundle + "/info.xml",
+    dataType: "xml",
+    success: function (xml) {
+        console.log("Beginning XML Parsing");
+        
+        // Parse the xml file and get data
             $(xml).find('packageInfo').each(function(){
-                   document.getElementById("packageTitle").innerHTML = $(this).find("name").text();
-                   document.getElementById("bundleId").innerHTML = $(this).find("bundleId").text();
-                   document.getElementById("version").innerHTML = $(this).find("version").text();
-                   document.getElementById("miniOS").innerHTML = $(this).find("miniOS").text();
-                   document.getElementById("maxiOS").innerHTML = $(this).find("maxiOS").text();
-
-                   $(xml).find('description').each(function(){
-                       $("#description" ).append('<li>' +$(this).text()+ '</li>');
-                   });
-
-                   $(xml).find('dependency').each(function(){
-                       $("#dependencies" ).append('<li>' +$(this).text()+ '</li>');
-                   });
-
-                   $(xml).find('linkName').each(function(){
-                       $("#dependencies" ).append('<li>' +$(this).text()+ '</li>');
-                   });
-
-                   $(xml).find('change').each(function(){
-                       $("#changeLog" ).append('<li>' + '<h1>' + $(this).find("changeVersion").text() + '</h1>');
-                       $(this).find('changeDescription').each(function(){
-                            $("#changeLog" ).append('<h2>' + $(this).text()+ '<h2>');
-                       });
-                       $("#changeLog" ).append('<li>');
-                   });
-
-
+            document.getElementById("name").innerHTML = $(this).find("name").text();
+            document.getElementById("bundleId").innerHTML = $(this).find("bundleId").text();
+            document.getElementById("version").innerHTML = $(this).find("version").text();
+            document.getElementById("miniOS").innerHTML = $(this).find("miniOS").text();
+            document.getElementById("maxiOS").innerHTML = $(this).find("maxiOS").text();
+            
+            $(xml).find('name').each(function(){
+                $("#name" ).append('<li>' +$(this).text()+ '</li>');
             });
-
-        }
+                $(xml).find('bundleId').each(function(){
+                    $("#bundleId" ).append('<li>' +$(this).text()+ '</li>');
+                });
+                $(xml).find('version').each(function(){
+                    $("#version" ).append('<li>' +$(this).text()+ '</li>');
+                });
+            
+            $(xml).find('description').each(function(){
+                $("#description" ).append('<li>' +$(this).text()+ '</li>');
+            });
+            
+            $(xml).find('dependency').each(function(){
+                $("#dependencies" ).append('<li>' +$(this).text()+ '</li>');
+            });
+            
+            $(xml).find('linkName').each(function(){
+                $("#dependencies" ).append('<li>' +$(this).text()+ '</li>');
+            });
+            
+            $(xml).find('change').each(function(){
+                $("#changeLog" ).append('<li>' + '<h1>' + $(this).find("changeVersion").text() + '</h1>');
+                $(this).find('changeDescription').each(function(){
+                    $("#changeLog" ).append('<h2>' + $(this).text()+ '<h2>');
+                });
+                $("#changeLog" ).append('<li>');
+            });
+            
+            
+        });
+        
+    }
     });
-
-
+    
+    
 });
 
 
